916aa404c8e18d2ab77f9d408606c5ffa403f297
Grace Kaliff <axel.kaliff@protonmail.com>
2022-04-05 15:44:15 +0200

    added support for comments

--- S2/parser.py
+++ S2/parser.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import enum
-# Using enum class create enumerations
+ # Using enum class create enumerations
 class Tokens(enum.Enum):
    ERROR = 0
    FORW = 1
@@ -16,6 +16,7 @@ class Tokens(enum.Enum):
    QUOTE = 9
    DECIMAL = 10
    HEX = 11
+   LEFT = 12
 
 def main():
 
@@ -25,35 +26,75 @@ def main():
          
         buffer = "" 
         last_c = ""
+        numBuffer = ""
+        hexBuffer = ""
+        commentLine = False
         
         while True:
              
             # Read from file
             c = f.read(1)
-
                
-            # new line works as space
-            if c == "\n":
+            # detect comment
+            if c == "%":
+                commentLine = True
+
+            if commentLine:
+                if c == "\n":
+                     commentLine = False
+                     continue
+                else: 
+                     continue
+                    
+
+
+
+            # all whitespace is equal
+            if "\n" in c or "\t" in c:
                 c = " "
 
-            
-            # does not add repeating whitespace
-            if not ((last_c == " ") and (c == " ")):
+            if c == " " and last_c == ".":
+                c == ""
+
+            if last_c == "." and (" " in c or "\n" in c or "\t" in c):
+                buffer = ""
                 
+
+           
+
+            # does not add repeating whitespace or whitespace after period
+            if not ((last_c == " ") and (c == " ")) or not (last_c == "." and c == " "):
                 buffer = buffer + c
                 
 
             # check if buffer matches any tokens
             if buffer == ".":
+
+                if not numBuffer == "":
+                    tokens.append(Tokens.DECIMAL)
+                    numBuffer = ""
+                if not hexBuffer == "":
+                    tokens.append(Tokens.HEX)
+                    hexBuffer = ""
+
                 tokens.append(Tokens.PERIOD)
                 buffer = ""
 
-            if buffer == "FORW ":
+            if buffer == "FORW " or buffer == " FORW ":
                 tokens.append(Tokens.FORW)
                 buffer = ""
 
-            if buffer == "BACK ":
-                tokens.append(Tokens.FORW)
+            if buffer == "LEFT " or buffer == " LEFT ":
+                tokens.append(Tokens.LEFT)
+                buffer = ""
+
+            if buffer == "RIGHT " or buffer == " RIGHT ":
+                tokens.append(Tokens.RIGHT)
+                buffer = ""
+
+
+            if buffer == "BACK " or buffer == " BACK ":
+                tokens.append(Tokens.BACK)
                 buffer = ""
 
             if buffer.strip() == "UP":
@@ -64,29 +105,43 @@ def main():
                 tokens.append(Tokens.DOWN)
                 buffer = ""
 
-            # TODO decimal
-
+            if c.isnumeric():
+                numBuffer = numBuffer + c
+                buffer = ""
 
             # TODO hex
+            
 
 
 
             # if there has been some sort of command
             if (c == " " or c == "\n") and not (buffer == "" or buffer == " " or buffer == "\n"):
-                tokens.append(Tokens.ERROR)
-                buffer = ""
+                # if there's a whitespace after something that wasn't a command (becuase the buffer would have been reset if it was)
 
+                # might have been a number or hex
 
+                if not numBuffer == "" and not c.isnumeric():
+                    tokens.append(Tokens.DECIMAL)
+                    numBuffer = ""
+
+                # TODO hex
+                else:
+                    print("e: ")
+                    print("'" + buffer + "'")
+
+                    tokens.append(Tokens.ERROR)
+                    buffer = ""
 
             last_c = c
             if not c:
                 break
          
-                # print the character
-                print("b")
-                print(buffer)
-                print(tokens)
-    print(tokens)
+    # print(tokens)
+    print("##################################")
+    for token in tokens:
+        print(token)
+        print(" ")
 
 if __name__=="__main__":
     main()
+
