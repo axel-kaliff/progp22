e37d871781684b948b9e973887c4fe1055bd20ed
oliveherb <olivia@herber.net>
2022-05-22 18:25:54 +0200

    Server and client can send messages

---
+++ inet/client.py
@@ -0,0 +1,46 @@
+import socket
+
+class Client:
+
+    def __init__(self, server):
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.port = 5555
+
+        self.server = server
+        self.addr = (self.server, self.port)
+
+
+    def connect(self):
+
+
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
+
+
+def main():
+
+    client = Client("192.168.1.124")
+
+    print(client.connect())
+
+    client.send("hi")
+    client.send("ho")
+
+
+if __name__ == "__main__":
+    main()
+
+
